861734cfdbaf72e3bd872ebb5562acc9d6ea530c
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-09-15 15:10:21 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
-
+//*Greetings
 app.get('/greeting/:name', function(req, res) {
   console.log(req.params)
   res.send('Wow! Hello there, ' + req.params.name);
+})
+
+//*Tip Calculator
+app.get('/tip/:total/:tipPercentage', function(req, res) {
+  console.log(req.params);
+  res.send('Your bill is $' + req.params.total + " and your tip percentage is " + req.params.tipPercentage + "% so you should tip" + "$" + (req.params.tipPercentage / req.params.total) * 100)
 })
 
 app.listen(3000, function () {
